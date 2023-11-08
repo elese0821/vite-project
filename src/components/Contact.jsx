978c0196edcacf74e979d5dd086e3a98cfd1d1c5
elese0821 <elese0821>
2023-11-08 12:29:30 +0900

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { contactText } from '../constants/index'
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">Contact</h2>
+
+        <div className="contact__lines top" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+
+          <div className="text">
+            {contactText.map((cont, key) => (
+              <div key={key}>
+                <a href="{cont.link}">{cont.title}</a>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="contact__lines " aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
